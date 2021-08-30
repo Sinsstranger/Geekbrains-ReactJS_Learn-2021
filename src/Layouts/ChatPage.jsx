95820eb5d25b550fbe44e87ffa034b0f212b5f74
sinstranger <sinstranger@yandex.ru>
2021-08-30 12:01:02 +0300

    Bot functional injected

--- src/Layouts/ChatPage.jsx
+++ src/Layouts/ChatPage.jsx
@@ -1,24 +1,36 @@
 import {Link, useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {useCallback, useEffect, useMemo} from "react";
+import uniqid from "uniqid";
 import {Room} from '../components/chat/room/Room'
 import {Form} from '../components/chat/Form'
 import './ChatPage.sass';
-import uniqid from "uniqid";
 
 export const ChatPage = (props) => {
 	const dispatch = useDispatch();
 	const {chatname} = useParams();
 	const rooms = useSelector(state => state.rooms);
+	const currentUser = useSelector(state => state.currentUser);
 	const messages = useMemo(() => {
 		return rooms.filter(room => room.link === chatname);
 	}, [chatname, rooms]);
+	const botAnswer = useCallback(() => {
+		let answerPeriod = () => setTimeout(() => {
+			dispatch({type: 'ADD_MESSAGE', chatname, payload: {id: uniqid(), user: `Bot`, text: "You message on Moderation"}});
+		}, 500);
+		if (currentUser === `Anonimous`) {
+			answerPeriod();
+			clearTimeout(answerPeriod);
+		}
+	}, [chatname, dispatch,currentUser])
 	useEffect(() => {
 		document.title = rooms.find(room => room.link === chatname) ? `Чат | ${rooms.find(room => room.link === chatname).anchor}` : 'Список чатов';
 	}, [chatname, rooms]);
+
 	const handleFormSend = useCallback((author, text) => {
 		dispatch({type: 'ADD_MESSAGE', chatname, payload: {id: uniqid(), user: author, text}});
-	}, [dispatch, chatname]);
+		botAnswer(author);
+	}, [dispatch, chatname, botAnswer]);
 	return (
 		<div className="chat-page">
 			{!props.onlyList ? <section className="chat-part">
--- src/components/chat/room/Room.js
+++ src/components/chat/room/Room.js
@@ -1,6 +1,6 @@
 import {Message} from '../Message/Message.js'
+
 export const Room = ({messages}) => {
-	console.log(messages)
 	return (
 		<section className="chat-room">
 			{messages.map(message => <Message key={message.id} message={message}/>)}
